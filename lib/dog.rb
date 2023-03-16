9e98c3f71c8386e42a5d74585b48b8c1115d5093
Gabriel <gabriel.wambugu@student.moringaschool.com>
2023-03-16 22:38:04 +0300

    Lab done

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,23 @@
 class Dog
+    def name=(name)
+       @name = name
+    def breed=(breed)
+       @breed = breed 
+    end
+    def name
+        @name
+    end
+    def breed
+        @breed
+    end
+end
 
 end
+
+
+
+fido = Dog.new
+fido.name = "Fido"
+
+snoopy = Dog.new
+snoopy.breed = "Beagle"
--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,19 @@
 class Person
-
+    def name=(name)
+        @name=name
+    def job=(job)
+        @job=(job)
+    end
+    def name
+        @name
+    end
+    def job
+        @job
+    end
 end
+end
+
+beyonce = Person.new
+beyonce.name = "Beyonce"
+
+beyonce.job = "Singer"
